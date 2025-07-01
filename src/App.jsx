66b52b95f7b0350a17388a36fe8c78e5fbb43053
vishnu <cmdvishnucp2018@gmail.com>
2025-07-01 12:24:51 +0530

    Clean setup without node_modules

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { useState,useEffect } from "react"
+function App(){
+
+  const[words,setWords]=useState([])
+  
+  useEffect(()=>{
+    fetch("https://dummyjson.com/quotes")
+    .then(res=>res.json())
+    .then((data)=>{
+      setWords(data.quotes.slice(0,10))
+    })
+  })
+
+  
+  return(
+
+  <div>
+    {words.map((x)=>(
+      <h1 style={{background:"rgb(236, 250, 255)"}} key={x.id}>{x.quote}</h1>
+    ))}
+  </div>
+  )
+}
+
+export default App
